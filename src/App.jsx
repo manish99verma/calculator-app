54dea01f1c124d60a2e4d8b46bbbf1cd63f34225
Manish Kumar Verma <manish6203993@gmail.com>
2024-03-21 15:02:56 +0530

    final deployement

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import Calculator from "./CalculatorComponent";
 
 function App() {
   return (
-    <>
+    <div className="app-view">
       <Calculator />
-    </>
+      <footer className="developed-by-text">
+        Developed by Manish Kumar Verma
+      </footer>
+    </div>
   );
 }
 
